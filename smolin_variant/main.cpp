432c5532543d72834e7d8cd534eced7524305ddc
Volodymyr Pavlenko <v.pavlenko@inango-systems.com>
2024-04-28 01:13:23 +0300

    Update docs

--- smolin_variant/main.cpp
+++ smolin_variant/main.cpp
@@ -8,9 +8,10 @@ using namespace std::chrono;
 int main()
 {
     SmolinVariant sv;
+    int1024_t num("670571563");
 
     auto start = high_resolution_clock::now();
-    auto [p, q] = sv.factorN(918092443);
+    auto [p, q] = sv.factorN(num);
     auto end = high_resolution_clock::now();
 
     duration<double, std::milli> duration_ms = end - start;
